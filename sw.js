5e6bdd3a963443f2d387d92b5e373a23c96f8716
arshittraders <arshittraders@gmail.com>
2026-07-15 22:27:58 -0700

    Bump SW cache version to force refresh (stale audit page cache)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='ddl-v2';
+const CACHE='ddl-v3';
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png']).catch(()=>{})));});
 self.addEventListener('activate',e=>{
   e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));
